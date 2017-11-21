51a6b82f8b7a06758d6ff065db725e7834ff6fe6
GabrielMMelo <gabrielmelocomp@gmail.com>
2017-11-21 09:40:02 -0200

    First commit

---
+++ p1.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+import sys
+
+def ler_arquivo():
+    nome_arquivo = sys.argv[1]
+    # Armazena quantidade de vertices e aresta e armazena o
+    # conteudo do arquivo em uma matriz de adjacencia
+    arquivo = open(nome_arquivo, 'r')
+
+    temp = arquivo.readline().split()
+    qt_vertice = int(temp[0])
+    qt_aresta = int(temp[1])
+
+    # Inicializa a matriz = 0
+    ma = [[0 for i in range(qt_vertice)] for j in range(qt_vertice)]
+
+    temp = 0
+    for linha in arquivo:
+    	temp = temp + 1
+    	valores = linha.split()
+    	if int(valores[0]) != 0 and int(valores[1]) != 0 and temp <= qt_aresta:
+    		ma[ int(valores[0])-1 ][ int(valores[1])-1 ] = int(valores[2])
+    		ma[ int(valores[1])-1 ][ int(valores[0])-1 ] = int(valores[2])
+
+    arquivo.close()
+
+    # Armazena o vertice de origem e destino, e a quantidade
+    # de pessoas
+    arquivo = open(nome_arquivo, 'r')
+
+    lista_linhas = arquivo.readlines()
+    qt_linhas = len(lista_linhas)
+    temp = lista_linhas[qt_linhas-2].split()
+
+    v_origem = int(temp[0])
+    v_destino = int(temp[1])
+    qt_pessoas = int(temp[2])
+
+    arquivo.close()
+
+    return ma, qt_vertice, qt_aresta, v_origem, v_destino, qt_pessoas
+
+	# TODO implementar o algoritmo de Ford Fulkerson que utilize como entrada uma matriz de adjacencia,
+	# um vertice de origem e um vertice de destino
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)	
+
+class GrafoBFS:
+	def __init__(self, ma, qt_vertice, qt_aresta, v_origem, v_destino, qt_pessoas):
+		self.ma = ma
+		self.adj = [[0 for i in range(qt_vertice)] for j in range(qt_vertice)]
+
+		for i in xrange(0,qt_vertice):
+			aux = 0
+			for j in xrange(0,qt_vertice):
+				if self.ma[i][j]!=0:
+					self.adj[i][aux]=j+1
+					aux= aux+1
+
+		self.qt_vertice = qt_vertice
+		self.qt_aresta = qt_aresta
+		self.v_origem = v_origem
+		self.v_destino = v_destino
+		self.qt_pessoas = qt_pessoas
+		self.cor = [0 for i in range(qt_vertice)]
+		self.dist = [0 for i in range(qt_vertice)]
+		self.pi = [0 for i in range(qt_vertice)]
+		for x in xrange(0,qt_vertice):				# FAZER LISTA, MAIS BONITO
+			self.cor[x] = "branco"
+		for x in xrange(0,qt_vertice):
+			self.dist[x] = 999999999
+		for x in xrange(0,qt_vertice):
+			self.pi[x] = -1
+
+def BuscaLargura(G,origem):
+    Q = Queue()
+    Q.enqueue(origem);
+    G.cor[origem-1] = "cinza"
+    G.dist[origem-1] = 0
+    while (Q.size() != 0):
+    	u = Q.dequeue();
+    	print(u)
+    	count = 0
+    	while (G.adj[u-1][count] != 0):
+    		v = G.adj[u-1][count]
+    		if (G.cor[v-1] == "branco"):
+    			G.cor[v-1] = "cinza"
+    			G.dist[v-1] = G.dist[u-1]+1
+    			G.pi[v-1] = u
+    			Q.enqueue(v)
+    			
+    		count = count + 1
+    	G.cor[u-1] = "preto"
+    print G.pi
+
+if __name__ == "__main__":
+	ma, qt_vertice, qt_aresta, v_origem, v_destino, qt_pessoas = ler_arquivo()
+	g = GrafoBFS(ma, qt_vertice, qt_aresta, v_origem, v_destino, qt_pessoas)
+	BuscaLargura(g,g.v_origem)
