86a282a830d7f29c59ff224ada27c07f98fecae6
brunoqs <brun_o13@hotmail.com>
2017-12-01 12:33:27 -0200

    gambiarra³

--- p2.py
+++ p2.py
@@ -2,19 +2,20 @@
 
 import sys
 import copy
+from collections import Counter
 
 def ler_arquivo():
 	nome_arquivo = sys.argv[1]
 
 	arquivo = open(nome_arquivo, 'r')
 	lista_linhas = arquivo.readlines()
-	print lista_linhas
 	
 	deposito = lista_linhas[1].replace('\n', '')
 
+	#porcaria
 	la = []
 	for linha in lista_linhas:
-		if len(linha.split()) == 4: # testa se e a linha do arquivo com as coordenadas
+		if len(linha.split()) == 4: # testa se e a linha do arquivo com as coord
 			temp = linha.split()
 			temp[0] = temp[0] + " " + temp[1] # junta 1 coordenada
 			del temp[1]
@@ -24,26 +25,40 @@ def ler_arquivo():
 			la.append(temp[::-1]) # espelhando grafo
 	print la
 
-	la1 = []
+	#separa coordenadas
+	coord = []
 	for linha in lista_linhas:
-		if len(linha.split()) == 4: # testa se e a linha do arquivo com as coordenadas
+		if len(linha.split()) == 4: # testa se e a linha do arquivo com as coord
 			temp = linha.split()
 			temp[0] = temp[0] + " " + temp[1] # junta 1 coordenada
 			del temp[1]
 			temp[1] = temp[1] + " " + temp[2] # junta 2 coordenada
 			del temp[2]
-			la1.append(temp[0])
-			la1.append(temp[1])
-	print la1
+			coord.append(temp[0])
+			coord.append(temp[1])
+	print coord
 
-	cont = 0
-	la2 = copy.deepcopy(la1)
-	for i in range(len(la1)):
-		for j in range(len(la1)):
-			if la1[i] == la1[j]:
-				la2[i] = i
-				la2[j] = i
-	print la2
+	#cria apelido para as coordenadas
+	apelido_coord = copy.deepcopy(coord)
+	for i in range(len(coord)):
+		for j in range(i, len(coord)):
+			if coord[i] == coord[j] and i != j:
+				apelido_coord[i] = i
+				apelido_coord[j] = i
+				del coord[j]
+			elif coord[i] != coord[j] and i != j:
+				apelido_coord[i] = i
+	print apelido_coord
+
+	#cria matriz de adjacencia
+	qt_vertice = len(Counter(apelido_coord).keys())
+	ma = [[0 for i in range(qt_vertice)] for j in range(qt_vertice)]
+
+	for i in range(len(apelido_coord)):
+		if i % 2 == 0:
+			ma[ apelido_coord[i] ][ apelido_coord[i+1] ] = 1
+			ma[ apelido_coord[i+1] ][ apelido_coord[i] ] = 1
+	print ma
 	
 if __name__ == "__main__":
 	ler_arquivo()
